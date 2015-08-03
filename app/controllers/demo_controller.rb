dacf05f8a44628614bf1406c8d426aac6253682e
Preston Edmands <pedmands@gmail.com>
2015-08-03 15:00:09 -0400

    added URL params to hello action in demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -9,6 +9,8 @@ class DemoController < ApplicationController
   def hello
 # 	  render('index')
 	@array = [1,2,3,4,5]
+	@id = params['id'].to_i
+	@page = params[:page].to_i
 	  end
   
   def other_hello
