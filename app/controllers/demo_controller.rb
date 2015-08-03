3c483e844c3605ab48462410f71ef7b7b49d92e5
Preston Edmands <pedmands@gmail.com>
2015-08-03 14:40:54 -0400

    Tinkerings with link_to helper

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,12 @@ class DemoController < ApplicationController
   layout false
   
   def index
-	  render('hello')
+	  
 	  end
   
   def hello
 # 	  render('index')
+	@array = [1,2,3,4,5]
 	  end
   
   def other_hello
