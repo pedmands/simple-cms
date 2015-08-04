0260b929b9b9f20d0fd582d91a5e2c51b348001b
Preston Edmands <pedmands@gmail.com>
2015-08-04 02:31:20 -0400

    renamed User object and table to AdminUser

---
+++ app/models/admin_user.rb
@@ -0,0 +1,17 @@
+class AdminUser < ActiveRecord::Base
+	# To configure a different table name:
+	# self.table_name = "admin_users"
+	
+# 	# short way
+# 	attr_accessor :first_name
+# 	
+# 	# long way
+# 	def last_name
+# 		@last_name
+# 	end
+# 	
+# 	def last_name=(value)
+# 		@last_name = value
+# 	end
+	
+end
