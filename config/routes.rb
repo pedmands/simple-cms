3e9b55f979874ba67fe7373befddbbb7a3ce4546
Preston Edmands <pedmands@gmail.com>
2015-08-08 12:46:54 -0400

    Added secure password for AdminUser with Blowfish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SimpleCms::Application.routes.draw do
 
   root "demo#index"
+  
+  get 'admin', :to => "access#index"
 
   #get "demo/index"
   match ':controller(/:action(/:id))', :via => [:get, :post]
