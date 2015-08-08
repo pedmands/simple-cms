779924042f6f428d1161c5e06697c42160c700b8
Preston Edmands <pedmands@gmail.com>
2015-08-08 15:27:46 -0400

    Added nested tree structure Subject >> Page >> Section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ class PagesController < ApplicationController
   layout "admin"
   
   before_action :confirm_logged_in
-
+	before_action :find_subject
+	
   def index
-    @pages = Page.sorted
+    # @pages = Page.where(:subject_id => @subject.id).sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -15,14 +17,14 @@ class PagesController < ApplicationController
   def new
 	  @subjects = Subject.order('position ASC')
 	  @page_count = Page.count + 1
-    @page = Page.new({:name => "Default"})
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
   end
 
   def create
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully."
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :subject_id => @subject.id)
     else
   	  @subjects = Subject.order('position ASC')
 		  @page_count = Page.count + 1
@@ -40,7 +42,7 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully."
-      redirect_to(:action => 'show', :id => @page.id)
+      redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
     else
 		  @subjects = Subject.order('position ASC')
 		  @page_count = Page.count
@@ -55,7 +57,7 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :subject_id => @subject.id)
   end
 
 
@@ -63,6 +65,12 @@ class PagesController < ApplicationController
 
     def page_params
       params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+    end
+    
+    def find_subject
+	    if params[:subject_id]
+		    @subject = Subject.find(params[:subject_id])
+	    end
     end
 
 end
